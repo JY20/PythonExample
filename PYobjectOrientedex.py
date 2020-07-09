d4c55d40a173a15bd85e7b2c3a028b4627ceb2a7
JY20 <43075399+JY20@users.noreply.github.com>
2020-07-08 21:43:49 -0400

    start the read and write txt file
    Working on the readd and write txt file of the data base. Also finish the basic UI of the program.

--- PYobjectOrientedex.py
+++ PYobjectOrientedex.py
@@ -1,3 +1,11 @@
+dataW  = open("data.txt", "w+") 
+dataR = open('data.txt', 'r')
+#for i in range(10):
+    #dataW.write("This is line %d\r\n" % (i+1))
+#dataW.close()
+contentsB = dataR.read()
+print(contentsB)
+
 class showsW:
     
     def __init__(self, name, typ, seasons, episodes):
@@ -9,9 +17,26 @@ class showsW:
         self.input = input("First letter of the piece of information you want to change")
         infoLet = ["n", "t", "s", "e"]
         for i in range (len(infoLet)):
-            if (lower(self.input) == infoLet[i]):
+            if (self.input.lower() == infoLet[i]):
                 print("sss")
     def get_info(self):
-        return self.name + "    " + self.typ + "    " + self.seasons + "    " + self.episodes
-d = showsW("dragonball", "anime", 1, 110)
-print(d.get_info())
+        return self.name + "    " + self.typ + "    " + str(self.seasons) + "    " + str(self.episodes)
+
+def clean():
+    dataW.truncate(0)
+    dataW.close()
+
+def inputs():
+    name = input("what is the show name" + "\n")
+    typ = input("what type of show is it" + "\n")
+    seasons = input("what season are you on" + "\n")
+    episodes = input("what episode are you on" + "\n")
+    try:
+        temp = str(name)
+        temp = str(typ)
+        temp = int(seasons)
+        temp = int(episodes)
+    except ValueError:
+        print("Error") 
+        inputs()
+inputs()
